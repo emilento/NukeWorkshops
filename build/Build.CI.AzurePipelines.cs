829b8f99d4f873a1fa4a5e15e60683ff30ca3688
Emil Nowak <emilento@gmail.com>
2024-06-05 13:36:28 +0200

    Add Nuke

---
+++ build/Build.CI.AzurePipelines.cs
@@ -0,0 +1,26 @@
+﻿using Nuke.Common.CI.AzurePipelines;
+
+[AzurePipelines(
+    suffix: "frontend",
+    AzurePipelinesImage.UbuntuLatest,
+    FetchDepth = 0,
+    PullRequestsDisabled = false,
+    InvokedTargets = [nameof(FrontendAll)],
+    NonEntryTargets = [nameof(FrontendClean), nameof(FrontendRestore), nameof(FrontendBuild)],
+    ExcludedTargets = [nameof(BackendClean), nameof(BackendBuild), nameof(BackendRestore), nameof(BackendTests), nameof(BackendTestsCodeCoverage), nameof(BackendAll)],
+    CacheKeyFiles = [],
+    CachePaths = []
+)]
+[AzurePipelines(
+    suffix: "backend",
+    AzurePipelinesImage.UbuntuLatest,
+    FetchDepth = 0,
+    PullRequestsDisabled = false,
+    InvokedTargets = [nameof(BackendAll)],
+    NonEntryTargets = [nameof(BackendClean), nameof(BackendRestore), nameof(BackendBuild), nameof(BackendTests), nameof(BackendTestsCodeCoverage)],
+    ExcludedTargets = [nameof(FrontendClean), nameof(FrontendRestore), nameof(FrontendBuild), nameof(FrontendAll)]
+)]
+partial class Build
+{
+
+}
